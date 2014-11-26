39cec88a97d77cade9ad880e353f634cd70ed3b4
Jonathan Hahn <jonathan.hahn@t-online.de>
2014-11-26 22:01:48 +0100

    behebt Fehler aus Buch

--- Algebraic Grid Generation/agg.m
+++ Algebraic Grid Generation/agg.m
@@ -1,14 +1,20 @@
 % Ränder definieren
 
-north = [0,5;1.5,5;2,5;4,5;5,5;10,5];
-east  = [10,1;10,2;10,3;10,5];
-south = [0,0;1,0;7,0;8,0;9,0;10,1];
-west  = [0,0;0,1;0,2.5;0,5];
+%north = [0,5;1.5,5;2,5;4,5;5,5;10,5];
+%east  = [10,1;10,2;10,3;10,5];
+%south = [0,0;1,0;7,0;8,0;9,0;10,1];
+%west  = [0,0;0,1;0,2.5;0,5];
 
-%north = [0,5;5,5;8,5;10,5];
-%east  = [10,0;9,2.5;10,5];
-%south = [0,0;5,0;8,1;10,0];
-%west  = [0,0;1,2.5;0,5];
+north = [0,5;5,5;8,5;10,5];
+east  = [10,0;9,2.5;10,5];
+south = [0,0;5,0;8,1;10,0];
+west  = [0,0;1,2.5;0,5];
+
+%north = [0,5;2.5,4;5,5];
+%east  = [5,0;5,5];
+%south = [0,0;2.5,0;5,0];
+%west  = [0,0;0,5];
+
 
 figure(1)
 clf;
@@ -31,7 +37,7 @@ for xi = [0:N]
     for eta = [0:M]
         % Interpolationsvorschrift S. 59
         pt = (1-eta/M)*xs(xi)+(eta/M)*xn(xi)+(1-xi/N)*xw(eta)+xi/N*xe(eta)...
-            - xi/N*(eta/M*xn(M)+(1-eta/M)*xs(M))-(1-xi/N)*(eta/M*xn(0)+(1-eta/M)*xs(0));
+            - xi/N*(eta/M*xn(N)+(1-eta/M)*xs(N))-(1-xi/N)*(eta/M*xn(0)+(1-eta/M)*xs(0));
         
         % Nur noch Plotkram
         ptsx(xi+1,eta+1) = pt(1);
