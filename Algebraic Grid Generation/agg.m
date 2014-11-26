92e81bb51cc5b5ad01b857881d46d04c713e1af2
Jonathan Hahn <jonathan.hahn@t-online.de>
2014-11-26 21:18:38 +0100

    behebt fehler in agg.m

--- Algebraic Grid Generation/agg.m
+++ Algebraic Grid Generation/agg.m
@@ -2,7 +2,7 @@
 
 north = [0,5;1.5,5;2,5;4,5;5,5;10,5];
 east  = [10,1;10,2;10,3;10,5];
-south = [0,0;1,0;7,0;8,1;9,1;10,1];
+south = [0,0;1,0;7,0;8,0;9,0;10,1];
 west  = [0,0;0,1;0,2.5;0,5];
 
 %north = [0,5;5,5;8,5;10,5];
@@ -14,8 +14,10 @@ figure(1)
 clf;
 hold on;
 
-M = size(north,1)-1;
-N = size(east,1)-1;
+
+% Größen nach Abbildung 3.10
+N = size(north,1)-1;
+M = size(east,1)-1;
 
 xs = @(xi) south(xi+1,:);
 xn = @(xi) north(xi+1,:);
@@ -25,19 +27,17 @@ xe = @(eta)east(eta+1,:);
 ptsx = ptsy = zeros(M+1,N+1);
 
 % logisches Gitter durch iterieren
-for xi = [0:M]
-    
-    cp = zeros(M+1,2);
-    
-    for eta = [0:N]
+for xi = [0:N]
+    for eta = [0:M]
         % Interpolationsvorschrift S. 59
         pt = (1-eta/M)*xs(xi)+(eta/M)*xn(xi)+(1-xi/N)*xw(eta)+xi/N*xe(eta)...
             - xi/N*(eta/M*xn(M)+(1-eta/M)*xs(M))-(1-xi/N)*(eta/M*xn(0)+(1-eta/M)*xs(0));
+        
+        % Nur noch Plotkram
         ptsx(xi+1,eta+1) = pt(1);
         ptsy(xi+1,eta+1) = pt(2);
-        plot(pt(1),pt(2),'r+');
         % Punkte plotten
-        disp([num2str([xi,eta]),'-',num2str(pt)])
+        plot(pt(1),pt(2),'r+');
         % Kanten plotten
         if(xi>0)
             % horizontale Linien plotten
