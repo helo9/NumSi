22413f8561f2710b33a9d3d859c97e5e586e79f4
Jonathan Hahn <jonathan.hahn@t-online.de>
2014-12-11 16:42:33 +0100

    adds solve function to Problem class

--- complete/Problem.py
+++ complete/Problem.py
@@ -1,7 +1,11 @@
 from Grid import Grid
+from numpy.linalg import solve as linsolve
 
 class Problem(Grid):
 	def createProblem(self,boundaries):
 		self.boundaries = boundaries
+	
+	def solve(self):
+		self.result = linsolve(self.A,self.b)
 
 
